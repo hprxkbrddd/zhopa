1b3b85f28744502cc30db220fb433ae7ccb95189
hprxkbrd <egor13ni@gmail.com>
2025-03-29 16:47:17 +0300

    Initial commit

---
+++ src/main/java/com/zhopa/ZhopaApplication.java
@@ -0,0 +1,13 @@
+package com.zhopa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhopaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZhopaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zhopa/ZhopaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhopa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhopaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
